7f99586d8da1c917fd630c7303aa1f7c70ab98c1
Amari <amaristhorne@outlook.com>
2025-11-17 22:45:22 -0500

    Created Project

---
+++ src/main/java/com/amaris/blackjack_simulation_project/BlackjackSimulationProjectApplication.java
@@ -0,0 +1,13 @@
+package com.amaris.blackjack_simulation_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackjackSimulationProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackjackSimulationProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amaris/blackjack_simulation_project/BlackjackSimulationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amaris.blackjack_simulation_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackjackSimulationProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
