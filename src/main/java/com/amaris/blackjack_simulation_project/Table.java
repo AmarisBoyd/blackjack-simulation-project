74a8f98ec17bd80d6317d1c1d1019713995e4e1e
Amari <amaristhorne@outlook.com>
2025-12-02 19:26:06 -0500

    Added functionality to read the deck file fill and shuffle the shoe

--- src/main/java/com/amaris/blackjack_simulation_project/Table.java
+++ src/main/java/com/amaris/blackjack_simulation_project/Table.java
@@ -2,13 +2,15 @@ package com.amaris.blackjack_simulation_project;
 //import arrays for array manipulation
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 //import collections for shuffling
 import java.util.Collections;
 
-import com.fasterxml.jackson.core.exc.StreamReadException;
+
+
+
 import com.fasterxml.jackson.databind.DatabindException;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 
@@ -16,9 +18,15 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class Table {
     //Variables to hold game state
     // Array to hold dealers hand using "Plenty big" approach to avoid dynamic resizing 
+    
     Card[] dealerHand = new Card[12];
-    //Array to hold "shoe" of cards (multiple decks)
+   
+    //Array to hold a single deck of cards for loading the shoe 
+    Card[] deck;
+     //Array to hold "shoe" of cards (multiple decks)
     Card[] shoe;
+    //Arraylist to hold the discard pile
+    ArrayList<Card> discard;
     //Array to hold players at the table
     Player[] players;
     TableRules rules;
@@ -30,13 +38,8 @@ public class Table {
 
     
 
-    //Method to get dealer's upcard value
-    public int getDealerScore() {
-        //return The value of the dealer's upcard which should be the second card in dealerHand
-        // (assuming first card is face down)
-        return this.dealerHand[1].getValue();
-        
-    }
+  
+   
     public void dealInitialCards() {
         //Deal two cards to each player and dealer from the shoe
         // Placeholder for loop through players and dealer to assign cards from shoe array
@@ -66,25 +69,96 @@ public class Table {
         this.shoe= newShoe;
 
     }
-    //method to read a json file that contains all the cards in a standard deck
-    //TODO fix this!! non functional read up on reading from JSON files again 
-    public void loadDeck() throws  DatabindException, IOException{
-        String src = "src/main/resources/Cards.json";
-        File file = new File(src);
-		Card[] deckCards = new Card[52];
-		ObjectMapper objectMapper = new ObjectMapper();
-        JsonNode node = objectMapper.readTree(file);
-        String suit = node.get("suit").asText();
-        String rank = node.get("rank").asText();
-        int value = node.get("numeric_value").asInt();
-        System.out.println("Suit"+ suit);
-        System.out.println("Rank"+rank);
-        System.out.println("Value"+value);
 
+    // Set the shoe size to number of decks* cards in a deck
+    public void initalizeShoeSize(){
+        this.shoe =  new Card[rules.getDeckNumber()*52];
+    }
+  
+    //method to read a json file that contains all the cards in a standard deck
+    
+    public void loadDeck() {
+        //location of cards config file make this selectable later
+        String src = "src/main/resources/Cards.json";
+        Card[] cards =null;
+        try {
+            File cardsFile = new File(src);
+            ObjectMapper objectMapper = new ObjectMapper();
+            cards =objectMapper.readValue(cardsFile, Card[].class);
+           this.setDeck(cards);
+        
+        }
+        catch(DatabindException d){
+            d.printStackTrace();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        
+       }
+
+
+    // load up the shoe based on max number of decks and cards in the "deck"
+    public void loadShoe(){
+        int shoeIndex=0;
+			for (int i = 0; i < this.rules.getDeckNumber(); i++) {
+				for(int k=0;k<52;k++){
+                this.shoe[shoeIndex]=deck[k];
+				shoeIndex++;}
+
+				
+			}
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    //Getters and setters banished to the near bottom like usual 
+
+    //Method to get dealer's upcard value
+     public int getDealerScore() {
+        //return The value of the dealer's upcard which should be the second card in dealerHand
+        // (assuming first card is face down)
+        return this.dealerHand[1].getValue();
+        
+    }
+
+    public void addPlayer(Player player){
+        this.players[0]=player;
+    }
+      public Card[] getShoe(){
+        return this.shoe;
+    }
+    
+    public void setDeck(Card[] deck) {
+        this.deck = deck;
+    }
+    public Card[] getDeck() {
+        return deck;
+    }
+
+   
+     
+        
+    
+    @Override
+    public String toString(){
+         StringBuilder shoeString = new StringBuilder("The shoe contains:\n");
+         for (Card card  : shoe) {
+            shoeString.append(card.toString()).append("\n");
+         }
+
+        return shoeString.toString();
+    }
 		
 	}
 
     
 
 
-}
