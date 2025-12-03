b8110fd66251fe2a0ab19b2c7d014b94f3d61129
Amari <amaristhorne@outlook.com>
2025-12-02 19:27:05 -0500

    Started one player test method

--- src/main/java/com/amaris/blackjack_simulation_project/BlackjackSimulationProjectApplication.java
+++ src/main/java/com/amaris/blackjack_simulation_project/BlackjackSimulationProjectApplication.java
@@ -1,22 +1,51 @@
 package com.amaris.blackjack_simulation_project;
 
+import java.awt.SystemTray;
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.fasterxml.jackson.databind.DatabindException;
+
 
 @SpringBootApplication
 public class BlackjackSimulationProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BlackjackSimulationProjectApplication.class, args);
-		//Simple test of strategy
-		testStrategy();
+		//disabling spring application so i can get the core functionality running before i mess with that nonsense 
+		//SpringApplication.run(BlackjackSimulationProjectApplication.class, args);
+		onePlayerTest();
+	
 	}
 	//method to test strategy with sample data
-
+	public static void onePlayerTest(){
+	Table table =new Table();
+		String shoePath ="src/main/resources/Shoe.txt";
+		String shuffledFile ="src/main/resources/Shuffled.txt";
+		Path shoeFile = Path.of(shoePath);
+		Path shuffledPath = Path.of(shuffledFile);
+		try {
+			// load the card data from the Cards.Json file
+			table.loadDeck();
+			// Initatlize how big the shoe is 
+			table.initalizeShoeSize();
+			// Load the shoe up with copies of the deck
+			table.loadShoe();
+			//Write current configuration of the shoe to a file for easier testing 
+			Files.writeString(shoeFile,table.toString());
+			//Shuffle the shoe before we start
+			table.shuffleShoe();
+			// Write results to another file to compare to make sure shuffle worked 
+			Files.writeString(shuffledPath,table.toString());
+			// create a player with default strategy
+			Player firstPlayer = new Player();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 	public static void testStrategy(){
 	// Test Player strategy method
 		//Add cards to dealer
