0118f9c7ace0ec0d9b55aca22a5ccc6e152b3fdf
Amari <amaristhorne@outlook.com>
2025-12-09 19:55:58 -0500

    Moved Test of player class to its own file

--- src/test/java/com/amaris/blackjack_simulation_project/BlackjackSimulationProjectApplicationTests.java
+++ src/test/java/com/amaris/blackjack_simulation_project/BlackjackSimulationProjectApplicationTests.java
@@ -10,45 +10,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BlackjackSimulationProjectApplicationTests {
 
+	// Helper function to make creating other test quicker since adding cards to hand is a bit tedious
 
-	@Test
-	void test_pair_strategy(){
-	// Test Player strategy method
-		//Add cards to dealer
-		Card dealerCard = new Card("8S",8);
-		//create test hand for player
-		Card[] testHand = {new Card("8 of Hearts",8), new Card("8 Of Diamonds",8)};
-		//Create player
-		Player player = new Player();
-		//Set player's hand for testing
-		player.handCards=testHand;
-		assertNotNull(player);
-		player.calcHandScore();
-		assertNotEquals(0,player.handScore);
-		assertEquals(16,player.handScore);
-		assertEquals(3, player.checkPairStrategy(dealerCard,player.handCards));
-	}
-	
-	@Test
-	 void test_Hard_Strategy(){
-		//Set player's hand for testing
-		Card dealerCard = new Card("8 of spades",8);
-		//create test hand for player
-		Card[] testHand = {new Card("10 of Hearts",10), new Card("8 Of Diamonds",8)};
-		//Create player
-		Player player = new Player();
-		//Set player's hand for testing
 
-		player.handCards=testHand;
-		assertNotNull(player.handCards);
-		player.calcHandScore();
-		assertNotEquals(0, player.handScore);
-		//Get strategy decision and print to console
-		
-		assertEquals(1,player.checkHardStrategy(dealerCard, testHand));
-		
-	}
-	//TODO Add soft strategy test
+
+
 	//TODO Move shoe creation and shuffle test here
 	//TODO Move 1 player test here
 }
