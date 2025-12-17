9dcc318f128f2524aa77c0e2a98be180313d8701
Amari <amaristhorne@outlook.com>
2025-12-16 19:30:20 -0500

    Reformated code for easier reading and implemented toString

--- src/main/java/com/amaris/blackjack_simulation_project/Player.java
+++ src/main/java/com/amaris/blackjack_simulation_project/Player.java
@@ -1,90 +1,89 @@
 package com.amaris.blackjack_simulation_project;
 
 
-
-
+import java.util.Arrays;
 
 /*
 Class to act as a "player"  holding the score of the hand  basic player will follow the basic strategy */
 public class Player {
     //Integer to track current hand for splits
-    protected  int currentHand =0; 
-    //Array to hold current hand's cards for easier reading
-    Card[] handCards;
+    protected int currentHand = 0;
     //Arrays to hold cards in hand multiple hands for splits for our each player will otherwise have only one hand
     //Default to max 4 hands with max 11 cards each (theoretical max in blackjack)
-    protected  Hand[] hands;
+    protected Hand[] hands;
     //Record of wins losses and pushes for player
-    protected  int wins =0;
-    protected  int losses =0;
-    protected  int pushes =0; 
+    protected int wins = 0;
+    protected int losses = 0;
+    protected int pushes = 0;
     //Integer to track hand score for strategy decisions
-    protected  int handScore=0;
+    protected int handScore = 0;
+    //Array to hold current hand's cards for easier reading
+    Card[] handCards;
     //Copy of table rules so the players can make some choices 
     TableRules rules;
-    
+
     //Default constructor Uses most common blackjack rules
     public Player() {
         // Make the default table rules 
         this.rules = new TableRules();
         //get max splits from the table rules add one to make total number of hands after splits
-         int maxHands= rules.getMaxSplits()+1;
+        int maxHands = rules.getMaxSplits() + 1;
         //create array to hold max 4 hands
         hands = new Hand[maxHands];
         //remember to initialize each hand in the array
-        for (int i=0; i<maxHands; i++) {
+        for (int i = 0; i < maxHands; i++) {
             hands[i] = new Hand();
         }
 
 
     }
-    // Create player based on given table rules 
+
+    // Create player based on given table rules
     public Player(TableRules rules) {
         //get max splits from the table rules add one to make total number of hands after splits
-        int maxHands= rules.getMaxSplits()+1;
+        int maxHands = rules.getMaxSplits() + 1;
         //create array to hold specified number of hands based on maxHands parameter
         hands = new Hand[maxHands];
         //remember to initialize each hand in the array
-        for (int i=0; i<maxHands; i++) {
-            hands[i] = new Hand();  }
+        for (int i = 0; i < maxHands; i++) {
+            hands[i] = new Hand();
+        }
 
     }
 
     //Method to decide what action to take based on dealer's card and player's hand
     public int strategy(Card dealerCard, int currentHand) {
-         // Int to hold "choice" of what to do 0=hit 1=stand 2=double 3=split default to hit
-         int decision;
-         //Array to hold current hand's cards for easier reading
-          handCards = this.hands[currentHand].getCards();
+        // Int to hold "choice" of what to do 0=hit 1=stand 2=double 3=split default to hit
+        int decision;
+        //Array to hold current hand's cards for easier reading
+        handCards = this.hands[currentHand].getCards();
         //Get current hand score
         this.handScore = this.hands[currentHand].getScore();
 
         //Check to see if pair 
-        if(handCards.length==2){
-         hands[currentHand].setIsPair(hands[currentHand].checkPair(handCards));
-       
+        if (handCards.length == 2) {
+            hands[currentHand].setIsPair(hands[currentHand].checkPair(handCards));
+
         }
-        
-    
+
+
         //Check for soft hand
         hands[currentHand].setIsSoft(hands[currentHand].checksoft(handCards));
-       
+
 
         if (hands[currentHand].getIsPair()) {
-        //check against pair strategy table
-        decision = checkPairStrategy(dealerCard, handCards);
-       }
-       else if (hands[currentHand].getIsSoft()) {
-        //check against soft hand strategy table
-        decision = checkSoftStrategy(dealerCard, handCards);  
-       }
-       else {
-        //check against hard hand strategy table
-        decision = checkHardStrategy(dealerCard, handCards);
-       }
+            //check against pair strategy table
+            decision = checkPairStrategy(dealerCard, handCards);
+        } else if (hands[currentHand].getIsSoft()) {
+            //check against soft hand strategy table
+            decision = checkSoftStrategy(dealerCard, handCards);
+        } else {
+            //check against hard hand strategy table
+            decision = checkHardStrategy(dealerCard, handCards);
+        }
         // Return the decision
-    
-       return decision;
+
+        return decision;
     }
 
 
@@ -93,89 +92,91 @@ public class Player {
         // Reminder 0=hit 1=stand 2=double 3=split 
 
         int[][] pairStrategyTableNoDouble = {
-            // Dealer's upcard: 2 3 4 5 6 7 8 9 10 A
-                         /*2*/ {0,0,3,3,3,3,0,0,0,0},
-                         /*3*/ {0,0,3,3,3,3,0,0,0,0},
-                         /*4*/ {0,0,0,0,0,0,0,0,0,0},
-                         /*5*/ {2,2,2,2,2,2,2,2,0,0},
-                         /*6*/ {0,3,3,3,3,0,0,0,0,0},
-                         /*7*/ {3,3,3,3,3,3,0,0,0,0},
-                         /*8*/ {3,3,3,3,3,3,3,3,3,3},
-                         /*9*/ {3,3,3,3,3,1,3,3,1,1},
-                         /*10*/{1,1,1,1,1,1,1,1,1,1},
-                         /*A*/ {3,3,3,3,3,3,3,3,3,3}
+                // Dealer's upcard: 2 3 4 5 6 7 8 9 10 A
+                /*2*/ {0, 0, 3, 3, 3, 3, 0, 0, 0, 0},
+                /*3*/ {0, 0, 3, 3, 3, 3, 0, 0, 0, 0},
+                /*4*/ {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                /*5*/ {2, 2, 2, 2, 2, 2, 2, 2, 0, 0},
+                /*6*/ {0, 3, 3, 3, 3, 0, 0, 0, 0, 0},
+                /*7*/ {3, 3, 3, 3, 3, 3, 0, 0, 0, 0},
+                /*8*/ {3, 3, 3, 3, 3, 3, 3, 3, 3, 3},
+                /*9*/ {3, 3, 3, 3, 3, 1, 3, 3, 1, 1},
+                /*10*/{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                /*A*/ {3, 3, 3, 3, 3, 3, 3, 3, 3, 3}
         };
         int[][] pairStrategyTableDoubleAfterSplit = {
-            // Dealer's upcard: 2 3 4 5 6 7 8 9 10 A
-                         /*2*/ {3,3,3,3,3,3,0,0,0,0},
-                         /*3*/ {3,3,3,3,3,3,0,0,0,0},
-                         /*4*/ {0,0,0,3,3,0,0,0,0,0},
-                         /*5*/ {2,2,2,2,2,2,2,2,0,0},
-                         /*6*/ {3,3,3,3,3,0,0,0,0,0},
-                         /*7*/ {3,3,3,3,3,3,0,0,1,1},
-                         /*8*/ {3,3,3,3,3,3,3,3,3,3},
-                         /*9*/ {3,3,3,3,3,1,3,3,1,1},
-                         /*10*/{1,1,1,1,1,1,1,1,1,1},
-                         /*A*/ {3,3,3,3,3,3,3,3,3,3}
+                // Dealer's upcard: 2 3 4 5 6 7 8 9 10 A
+                /*2*/ {3, 3, 3, 3, 3, 3, 0, 0, 0, 0},
+                /*3*/ {3, 3, 3, 3, 3, 3, 0, 0, 0, 0},
+                /*4*/ {0, 0, 0, 3, 3, 0, 0, 0, 0, 0},
+                /*5*/ {2, 2, 2, 2, 2, 2, 2, 2, 0, 0},
+                /*6*/ {3, 3, 3, 3, 3, 0, 0, 0, 0, 0},
+                /*7*/ {3, 3, 3, 3, 3, 3, 0, 0, 1, 1},
+                /*8*/ {3, 3, 3, 3, 3, 3, 3, 3, 3, 3},
+                /*9*/ {3, 3, 3, 3, 3, 1, 3, 3, 1, 1},
+                /*10*/{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                /*A*/ {3, 3, 3, 3, 3, 3, 3, 3, 3, 3}
         };
-        if(this.rules.getDoubleAfterSplit())
-            {return pairStrategyTableDoubleAfterSplit[hand[0].getValue()-2][dealerCard.getValue()-2];}
-        else{
-        // Return the action from the pair strategy table
-        return pairStrategyTableNoDouble[hand[0].getValue()-2][dealerCard.getValue()-2];}
+        if (this.rules.getDoubleAfterSplit()) {
+            return pairStrategyTableDoubleAfterSplit[hand[0].getValue() - 2][dealerCard.getValue() - 2];
+        } else {
+            // Return the action from the pair strategy table
+            return pairStrategyTableNoDouble[hand[0].getValue() - 2][dealerCard.getValue() - 2];
+        }
     }
 
-    
+
     // Method to check soft hand strategy
     protected int checkSoftStrategy(Card dealerCard, Card[] hand) {
         // Table for soft hand strategy 
         // reminder 0=hit 1=stand 2=double 3=split(should not occur in soft strategy)
-       
+
         int[][] softStrategyTable = {
-            // Dealer's upcard:
-            //      2 3 4 5 6 7 8 9 10 A
-            /*13*/ {0,0,0,2,2,0,0,0,0,0},
-            /*14*/ {0,0,0,2,2,0,0,0,0,0},
-            /*15*/ {0,0,2,2,2,0,0,0,0,0},
-            /*16*/ {0,0,2,2,2,0,0,0,0,0},
-            /*17*/ {1,2,2,2,2,1,1,0,0,0},
-            /*18*/ {1,1,1,1,1,1,1,1,0,0},
-            /*19*/ {1,1,1,1,1,1,1,1,1,1},
-            /*20*/ {1,1,1,1,1,1,1,1,1,1}
+                // Dealer's upcard:
+                //      2 3 4 5 6 7 8 9 10 A
+                /*13*/ {0, 0, 0, 2, 2, 0, 0, 0, 0, 0},
+                /*14*/ {0, 0, 0, 2, 2, 0, 0, 0, 0, 0},
+                /*15*/ {0, 0, 2, 2, 2, 0, 0, 0, 0, 0},
+                /*16*/ {0, 0, 2, 2, 2, 0, 0, 0, 0, 0},
+                /*17*/ {1, 2, 2, 2, 2, 1, 1, 0, 0, 0},
+                /*18*/ {1, 1, 1, 1, 1, 1, 1, 1, 0, 0},
+                /*19*/ {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                /*20*/ {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
         };
         /*Return based on soft hand strategy table 
         Subtract 13 from hand score to get correct row and dealer card value -2 for correct column
         */
-        return softStrategyTable[handScore - 13][dealerCard.getValue()-2];
-          }
+        return softStrategyTable[handScore - 13][dealerCard.getValue() - 2];
+    }
+
     // Method to check hard hand strategy
     protected int checkHardStrategy(Card dealerCard, Card[] hand) {
         // Table for hard hand strategy Starts at 8 because the table only covers 8-20
-    
-        if(handScore<8){
+
+        if (handScore < 8) {
             return 0; //always hit under should be the only place this occurs 
         }
         int[][] hardStrategyTable = {
-            // Dealer's upcard: 
-            /*      2 3 4 5 6 7 8 9 10 A*/
-            /*8*/  {0,0,0,0,0,0,0,0,0,0},
-            /*9*/  {0,2,2,2,2,0,0,0,0,0},
-            /*10*/ {2,2,2,2,2,2,2,2,0,0},
-            /*11*/ {2,2,2,2,2,2,2,2,2,2},
-            /*12*/ {0,0,1,1,1,0,0,0,0,0},
-            /*13*/ {1,1,1,1,1,0,0,0,0,0},
-            /*14*/ {1,1,1,1,1,0,0,0,0,0},
-            /*15*/ {1,1,1,1,1,0,0,0,0,0},
-            /*16*/ {1,1,1,1,1,0,0,0,0,0},
-            /*17*/ {1,1,1,1,1,1,1,1,1,1},
-            /*18*/ {1,1,1,1,1,1,1,1,1,1},
-            /*19*/ {1,1,1,1,1,1,1,1,1,1},
-            /*20*/ {1,1,1,1,1,1,1,1,1}
-        
+                // Dealer's upcard:
+                /*      2 3 4 5 6 7 8 9 10 A*/
+                /*8*/  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                /*9*/  {0, 2, 2, 2, 2, 0, 0, 0, 0, 0},
+                /*10*/ {2, 2, 2, 2, 2, 2, 2, 2, 0, 0},
+                /*11*/ {2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
+                /*12*/ {0, 0, 1, 1, 1, 0, 0, 0, 0, 0},
+                /*13*/ {1, 1, 1, 1, 1, 0, 0, 0, 0, 0},
+                /*14*/ {1, 1, 1, 1, 1, 0, 0, 0, 0, 0},
+                /*15*/ {1, 1, 1, 1, 1, 0, 0, 0, 0, 0},
+                /*16*/ {1, 1, 1, 1, 1, 0, 0, 0, 0, 0},
+                /*17*/ {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                /*18*/ {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                /*19*/ {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                /*20*/ {1, 1, 1, 1, 1, 1, 1, 1, 1}
+
         };
-          //Return based on hard hand strategy table subtract 8 from hand score to get correct row and dealer card value -2 for correct column
+        //Return based on hard hand strategy table subtract 8 from hand score to get correct row and dealer card value -2 for correct column
         return hardStrategyTable[handScore - 8][dealerCard.getValue() - 2];
-          }
+    }
 
     //Debug helper  method to add cards to hand and see other functions work properly
     public void debugSetHand(Card[] hand) {
@@ -183,17 +184,40 @@ public class Player {
         this.hands[currentHand].setCards(hand);
         System.out.println(this.hands[currentHand].toString());
 
-        
+
     }
-    public void calcHandScore(){
+
+    public void calcHandScore() {
         //set to 0 to avoid multiple calls to this function adding more than needed
         this.handScore = 0;
         for (Card card : handCards) {
-            this.handScore+=card.getValue();
+            this.handScore += card.getValue();
         }
 
     }
 
 
-    
+    public void dealCard(Card card) {
+        //Add the card given by the dealer to the current working hand
+        this.hands[currentHand].addCard(card);
+    }
+
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "currentHand=" + currentHand +
+                ", handCards=" + Arrays.toString(handCards) +
+                ", hands=" + Arrays.toString(hands) +
+                ", wins=" + wins +
+                ", losses=" + losses +
+                ", pushes=" + pushes +
+                ", handScore=" + handScore +
+                ", rules=" + rules +
+                '}';
+    }
+
+    public Hand[] getHand() {
+        return hands;
+    }
 }
