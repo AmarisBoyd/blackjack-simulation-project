61a7a287ac078fb9d635ff4961ebd3132f236ef3
Amari <amaristhorne@outlook.com>
2025-12-16 19:26:37 -0500

    Reformated code for easier reading

--- src/main/java/com/amaris/blackjack_simulation_project/Card.java
+++ src/main/java/com/amaris/blackjack_simulation_project/Card.java
@@ -1,63 +1,69 @@
 package com.amaris.blackjack_simulation_project;
+
 //
 public class Card {
     //String to hold suit and rank of card 
-    String suit="";
-    String rank="";
+    String suit = "";
+    String rank = "";
     String suitAndRank;
-    
+
     //Integer to hold value of card
     int value;
     boolean toString;
-    public Card(){
-      this.suit="";
-      this.rank ="";
-      this.value=0;
-      this.suitAndRank="";
+
+    public Card() {
+        this.suit = "";
+        this.rank = "";
+        this.value = 0;
+        this.suitAndRank = "";
     }
+
     //Constructor to initialize suitAndRank and value
     public Card(String suitAndRank, int value) {
-    this.suitAndRank = suitAndRank;
-    this.value = value;     }
-    public Card(String suit,String rank, int value) {
-    this.suit=suit;
-    this.rank=rank;
-    this.value = value;  
-    this.suitAndRank = suit+" "+rank;
-  }
- 
-
-   public void setSuit(String suit) {
-        this.suit = suit;
+        this.suitAndRank = suitAndRank;
+        this.value = value;
     }
 
-  
+    public Card(String suit, String rank, int value) {
+        this.suit = suit;
+        this.rank = rank;
+        this.value = value;
+        this.suitAndRank = suit + " " + rank;
+    }
 
-    public void setRank(String rank) {
-        this.rank = rank;}
-        
-    //Getter method for Rank and suit 
+    //Getter method for Rank and suit
     public int getValue() {
         return this.value;
     }
+
     //Getter method for value
     public String getSuitAndRank() {
         return this.suitAndRank;
     }
-    //toString method for easy printing
-    
-      public String getRank() {
+
+    public String getRank() {
         return rank;
     }
-        public String getSuit() {
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+    //toString method for easy printing
+
+    public String getSuit() {
         return suit;
+    }
+
+    public void setSuit(String suit) {
+        this.suit = suit;
     }
 
     @Override
     public String toString() {
-       this.suitAndRank= this.suit+" "+this.rank;
-       return this.suitAndRank + ": " + this.value;
-       
+        this.suitAndRank = this.suit + " " + this.rank;
+        return this.suitAndRank + ": " + this.value;
+
     }
-    
+
+
 }
