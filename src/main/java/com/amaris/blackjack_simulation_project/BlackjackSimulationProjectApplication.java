369f7ff5e962786ce00f2006e6ce2916397efa79
Amari <amaristhorne@outlook.com>
2025-12-16 19:25:35 -0500

    Updated TODO with new objectives

--- src/main/java/com/amaris/blackjack_simulation_project/BlackjackSimulationProjectApplication.java
+++ src/main/java/com/amaris/blackjack_simulation_project/BlackjackSimulationProjectApplication.java
@@ -1,27 +1,22 @@
 package com.amaris.blackjack_simulation_project;
 
-import java.awt.SystemTray;
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
 public class BlackjackSimulationProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BlackjackSimulationProjectApplication.class, args);
+    static void main(String[] args) {
+        SpringApplication.run(BlackjackSimulationProjectApplication.class, args);
+        //TODO Clean up the table/Dealer relationship
+        //TODO Implement playerActions
+        //TODO Implement dealerActions
+        //TODO Implement checkTableState
+        //TODO Implement cleanTable
 
-	
-	}
 
+    }
 
-	
-	
 
 }
