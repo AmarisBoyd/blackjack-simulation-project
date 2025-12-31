913f95dcd96e45d135bf816340dfd9c87b4194b0
Amari <amaristhorne@outlook.com>
2025-12-30 20:09:59 -0500

    Refactored to create a base "person" class instead of having dealer extend player

--- src/main/java/com/amaris/blackjack_simulation_project/Dealer.java
+++ src/main/java/com/amaris/blackjack_simulation_project/Dealer.java
@@ -1,10 +1,18 @@
 package com.amaris.blackjack_simulation_project;
 
-public class Dealer extends Player {
+public class Dealer extends Person {
     private Hand dealerHand;
     public Dealer() {
         this.dealerHand = new Hand();
-        super();
+
+    }
+
+    @Override
+    public void dealCard(Card card) {
+        //add the card to the hand
+        this.dealerHand.addCard(card);
+        //update the current score
+        this.handScore=this.dealerHand.getScore();
     }
 
     public int strategy(){
@@ -18,6 +26,7 @@ public class Dealer extends Player {
         return 0;
     }
     public void checkTableState(Table table){
+
         // at the end of the hand check who won and lost
 
     }
@@ -38,7 +47,9 @@ public class Dealer extends Player {
     public void setDealerHand(Hand dealerHand) {
         this.dealerHand = dealerHand;
     }
-    public Card getUpcard(){
+
+
+    public Card getUpCard(){
         return dealerHand.getCards()[0];
     }
 }
---
+++ src/main/java/com/amaris/blackjack_simulation_project/Person.java
@@ -0,0 +1,14 @@
+package com.amaris.blackjack_simulation_project;
+//Abstract class that both dealer and player will extend
+public abstract class Person {
+    Card [] hand;
+    protected int handScore;
+    TableRules rules;
+    Person(){
+        this.handScore = 0;
+
+    }
+    public abstract void dealCard(Card card);
+
+
+}
