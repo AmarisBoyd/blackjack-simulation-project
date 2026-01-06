1ff5b4c8afd747749b7c7b5df9f39f1f9c641c63
Amari <amaristhorne@outlook.com>
2026-01-05 19:16:38 -0500

    Added burn card and clear bust methods to Dealer

--- src/main/java/com/amaris/blackjack_simulation_project/Dealer.java
+++ src/main/java/com/amaris/blackjack_simulation_project/Dealer.java
@@ -1,5 +1,8 @@
 package com.amaris.blackjack_simulation_project;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Dealer extends Person {
     private Hand dealerHand;
     public Dealer() {
@@ -51,5 +54,27 @@ public class Dealer extends Person {
 
     public Card getUpCard(){
         return dealerHand.getCards()[0];
+    }
+
+    public void burnCard(ArrayList<Card> shoe, ArrayList<Card> discard) {
+        //add the last card of the shoe to the discard
+        discard.add(shoe.getLast());
+        //remove last card of the shoe from
+        shoe.remove(shoe.getLast());
+    }
+    //Clear up cards from player and check if they are still in the round
+    public void clearBust(Player currentPlayer, ArrayList<Card> discard) {
+        // get the current hand for readability
+        int currentHand = currentPlayer.getCurrentHand();
+        //add the hand that bust to the discard
+        discard.addAll(List.of(currentPlayer.getHand()[currentHand].getCards()));
+        //clear the players hand
+        currentPlayer.getHand()[currentHand]=new Hand();
+        //check if the player has split
+        if(!currentPlayer.isHasSplit()){
+            //if they haven't then they only had this hand so they have bust and are out of the game
+            currentPlayer.setHasBust(true);
+        }
+
     }
 }
