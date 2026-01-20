5820875f2978efd2e133d1788a3f8866959420ca
Amari <amaristhorne@outlook.com>
2026-01-20 00:18:32 -0500

    Created TODO's in application test

--- src/test/java/com/amaris/blackjack_simulation_project/BlackjackSimulationProjectApplicationTests.java
+++ src/test/java/com/amaris/blackjack_simulation_project/BlackjackSimulationProjectApplicationTests.java
@@ -1,16 +1,12 @@
 package com.amaris.blackjack_simulation_project;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BlackjackSimulationProjectApplicationTests {
 
-	
+    //TODO Logic of changing aces from 11 to 1 needs to be fixed creates index out of bound error
+    //TODO figure out why sometimes the dealer draws infinitely
 
 
 
