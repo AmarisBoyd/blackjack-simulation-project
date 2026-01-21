fb06761ae758fc5e09270bc7ff1cac1ecd4ffa05
Amari <amaristhorne@outlook.com>
2026-01-20 20:44:23 -0500

    Finished the TODO's so removed them

--- src/test/java/com/amaris/blackjack_simulation_project/BlackjackSimulationProjectApplicationTests.java
+++ src/test/java/com/amaris/blackjack_simulation_project/BlackjackSimulationProjectApplicationTests.java
@@ -5,10 +5,5 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BlackjackSimulationProjectApplicationTests {
 
-    //TODO Logic of changing aces from 11 to 1 needs to be fixed creates index out of bound error
-    //TODO figure out why sometimes the dealer draws infinitely
-
-
-
 
 }
