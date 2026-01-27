6675ec32c9e7249c8549d4e24458001b0c28cb14
Amari <amaristhorne@outlook.com>
2026-01-27 13:51:28 -0500

    Changed to a strategy instead of a player

--- src/main/java/com/amaris/blackjack_simulation_project/NoBustPlayer.java
+++ src/main/java/com/amaris/blackjack_simulation_project/NoBustPlayer.java
@@ -1,35 +1,29 @@
 package com.amaris.blackjack_simulation_project;
 
-import java.util.ArrayList;
-
 //Class for a player that follows a no-bust strategy (yes, they exist ive seen them)
-public class NoBustPlayer extends Player {
-
+public class NoBustPlayer implements Strategy {
 
     @Override
-    protected int checkHardStrategy(Card dealerCard, ArrayList<Card> hand) {
-        if (this.handScore < 8) {
-            return 0; //always hit under should be the only place this occurs 
-        }
-        //No bust players will not hit on hands 12 and above
-        if (this.handScore >= 12) {
-            return 1; //stand
-        }
-        //Otherwise follow basic hard strategy(table is a bit overkill for this but whatever keeps things consistent)
-        int[][] hardStrategyTable = {
-                // Dealer's upcard: 2 3 4 5 6 7 8 9 10 A
-                /*5*/ {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                /*6*/ {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                /*7*/ {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                /*8*/ {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                /*9*/ {2, 2, 2, 2, 2, 0, 0, 0, 0, 0},
-                /*10*/{2, 2, 2, 2, 2, 2, 2, 2, 0, 0},
-                /*11*/{2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
-        };
-        //Return based on hard hand strategy table
-        return hardStrategyTable[this.handScore - 5][dealerCard.getValue() - 2];
-
+    public BlackjackAction checkSoftStrategy(Card dealerCard, Hand hand) {
+        return null;
     }
+
+    @Override
+    public BlackjackAction checkHardStrategy(Card dealerCard, Hand hand) {
+        return null;
+    }
+
+    @Override
+    public BlackjackAction checkPairStrategy(Card dealerCard, Hand hand) {
+        return null;
+    }
+
+    @Override
+    public BlackjackAction checkSurrenderStrategy(Card dealerCard, Hand hand) {
+        return null;
+    }
+
+
     //Soft strategy not needed for no-bust player since they will never have a soft hand over 11
     //Pair strategy follows normal rules as no-bust player can still split
     //Double strategy follows normal rules as no-bust 
