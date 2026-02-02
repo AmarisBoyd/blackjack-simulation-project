26ff2fda0da3617123184c20e88ef19ee3fdb2a7
Amari <amaristhorne@outlook.com>
2026-02-02 11:24:23 -0500

    Added that it can throw exceptions

--- src/main/java/com/amaris/blackjack_simulation_project/Strategy.java
+++ src/main/java/com/amaris/blackjack_simulation_project/Strategy.java
@@ -1,11 +1,11 @@
 package com.amaris.blackjack_simulation_project;
 
 public interface Strategy {
-    BlackjackAction checkSoftStrategy(Card dealerCard, Hand hand);
+    BlackjackAction checkSoftStrategy(Card dealerCard, Hand hand) throws Exception;
 
-    BlackjackAction checkHardStrategy(Card dealerCard, Hand hand);
+    BlackjackAction checkHardStrategy(Card dealerCard, Hand hand) throws Exception;
 
-    BlackjackAction checkPairStrategy(Card dealerCard, Hand hand);
+    BlackjackAction checkPairStrategy(Card dealerCard, Hand hand) throws Exception;
 
     BlackjackAction checkSurrenderStrategy(Card dealerCard, Hand hand);
 
