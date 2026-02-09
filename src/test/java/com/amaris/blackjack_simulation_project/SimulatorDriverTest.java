f654256e88aeef3138d4cb45243786a07e350a36
Amari <amaristhorne@outlook.com>
2026-02-09 15:11:46 -0500

    Disabled integration test so they are not run on build

--- src/test/java/com/amaris/blackjack_simulation_project/SimulatorDriverTest.java
+++ src/test/java/com/amaris/blackjack_simulation_project/SimulatorDriverTest.java
@@ -2,6 +2,7 @@ package com.amaris.blackjack_simulation_project;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class SimulatorDriverTest {
@@ -13,17 +14,21 @@ public class SimulatorDriverTest {
 
     }
 
-    //TODO
+
     @Test
+
+    @Disabled
     void one_Player_Stress_Test() throws Exception {
-        String[] driverArgs = {"1", "300", "src/main/Test_Results.txt"};
+        String[] driverArgs = {"1", "8000", "src/main/Test_Results.txt"};
         try {
             SimulatorDriver.driver(driverArgs);
+
         } catch (Exception e) {
             System.err.println(e.getMessage());
         }
     }
 
+    @Disabled
     @Test
     void driverTestThrowsIllegalArgumentException() throws Exception {
         String[] driverArgs = {"1", "300", "src/main/Test_Results.txt", "2"};
